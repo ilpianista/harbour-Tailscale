cba5595cd44e849cf8f1d8e896525bc2b7c825bf
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2025-11-15 01:38:16 +0100

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Harbour Tailscale/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-tailscale/harbour-tailscale/es/

--- translations/harbour-tailscale-es.ts
+++ translations/harbour-tailscale-es.ts
@@ -5,37 +5,37 @@
     <name>About</name>
     <message>
         <source>Tailscale version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión de Tailscale</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>En funcionamiento</translation>
     </message>
     <message>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconectado</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>Tailscale status</source>
-        <translation type="unfinished"></translation>
+        <translation>Estado de Tailscale</translation>
     </message>
     <message>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Desconectado</translation>
     </message>
     <message>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>En funcionamiento</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
 </context>
 </TS>
