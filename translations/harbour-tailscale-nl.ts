0919f07f7e44ddde333f2be982731f6185ab16e4
Nathan <nthn@unseen.is>
2026-08-23 15:01:59 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: Harbour Tailscale/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-tailscale/translations/nl/

--- translations/harbour-tailscale-nl.ts
+++ translations/harbour-tailscale-nl.ts
@@ -5,57 +5,57 @@
     <name>About</name>
     <message>
         <source>Tailscale version</source>
-        <translation type="unfinished"></translation>
+        <translation>Tailscale-versie</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Actief</translation>
     </message>
     <message>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Inactief</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Inactief</translation>
     </message>
     <message>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Actief</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Over</translation>
     </message>
     <message>
         <source>This device</source>
-        <translation type="unfinished"></translation>
+        <translation>Dit apparaat</translation>
     </message>
     <message>
         <source>Online</source>
-        <translation type="unfinished"></translation>
+        <translation>Online</translation>
     </message>
     <message>
         <source>Seen </source>
-        <translation type="unfinished"></translation>
+        <translation>Gezien </translation>
     </message>
     <message>
         <source>Tailscale</source>
-        <translation type="unfinished"></translation>
+        <translation>Tailscale</translation>
     </message>
     <message>
         <source>No devices found</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen apparaten gevonden</translation>
     </message>
     <message>
         <source>Pull down to bring Tailscale up</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek omlaag om Tailscale te activeren</translation>
     </message>
 </context>
 </TS>
